f1d3a8401d84f4951724ded145822eddd8c06357
Gojo-Bo <omarmohamedhossny80@gmail.com>
2024-09-19 13:22:30 +0300

    Update pinterest.js

--- plugins/pinterest.js
+++ plugins/pinterest.js
@@ -1,1 +1,14 @@
+import { pinterest } from '@bochilteam/scraper'
 
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw `✳️ اكتب الي عايز تدور عليه وياريت انجليزي عشان يتطبق الامر صح ✅?\n📌 مثال  : ${usedPrefix + command} ITACHI`
+  const json = await pinterest(text)
+  conn.sendFile(m.chat, json.getRandom(), 'pinterest.jpg', `
+*تم💌:*  ${text}
+`.trim(), m)
+}
+handler.help = ['pinterest']
+handler.tags = ['img']
+handler.command = ['بين'] 
+
+export default handler
