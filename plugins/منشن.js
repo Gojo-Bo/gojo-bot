67b9801c6e5c1a4e2e5738d641e95f04b193a387
Gojo-Bo <omarmohamedhossny80@gmail.com>
2024-09-19 23:53:10 +0300

    Update منشن.js

--- plugins/منشن.js
+++ plugins/منشن.js
@@ -1,1 +1,47 @@
+const _0x12c300 = _0x8f22;
+(function (_0x8900ee, _0x5614a9) {
+    const _0x164fe3 = _0x8f22,
+        _0x4beb2b = _0x8900ee();
+    while (!![]) {
+        try {
+            const _0xb5a68b = parseInt(_0x164fe3(0x1e8)) / 0x1 + -parseInt(_0x164fe3(0x1eb)) / 0x2 * (-parseInt(_0x164fe3(0x1e0)) / 0x3) + -parseInt(_0x164fe3(0x1f0)) / 0x4 * (-parseInt(_0x164fe3(0x1d9)) / 0x5) + parseInt(_0x164fe3(0x1e7)) / 0x6 * (parseInt(_0x164fe3(0x1ec)) / 0x7) + parseInt(_0x164fe3(0x1e6)) / 0x8 * (parseInt(_0x164fe3(0x1dc)) / 0x9) + -parseInt(_0x164fe3(0x1e4)) / 0xa * (-parseInt(_0x164fe3(0x1dd)) / 0xb) + -parseInt(_0x164fe3(0x1de)) / 0xc;
+            if (_0xb5a68b === _0x5614a9) break;
+            else _0x4beb2b['push'](_0x4beb2b['shift']());
+        } catch (_0x380364) {
+            _0x4beb2b['push'](_0x4beb2b['shift']());
+        }
+    }
+}(_0x1fa7, 0x4f566));
 
+function _0x1fa7() {
+    const _0x5d8ba6 = ['7rJyhGM', '*✦━━━━[ Gojo ]━━━━✦*\x0a*المنشن الجماعي*\x0a\x0a', 'filter', 'map', '120cIfLaV', 'replace', '\x0a*✦━━━━[ ALMOHREZ ]━━━━✦*', '67165MxVTtl', 'admin', 'reply', '694836vvImgf', '7821yQBjJo', '18387204XohjDX', 'help', '18519OsBgFA', 'منشن', 'join', 'jid', '1880MwLSAj', ' منشن 🫶🏻 ❤️ @', '32nwoMUe', '2905098uvxJBi', '231272LdnnzA', 'group', 'command', '96GGigVT'];
+    _0x1fa7 = function () {
+        return _0x5d8ba6;
+    };
+    return _0x1fa7();
+}
+
+function _0x8f22(_0x1e14cb, _0x3fd0c3) {
+    const _0x1fa7a6 = _0x1fa7();
+    return _0x8f22 = function (_0x8f22df, _0x47834a) {
+        _0x8f22df = _0x8f22df - 0x1d8;
+        let _0x50f34c = _0x1fa7a6[_0x8f22df];
+        return _0x50f34c;
+    }, _0x8f22(_0x1e14cb, _0x3fd0c3);
+}
+let handler = async (_0x1a4457, {
+    conn: _0x1abe00,
+    text: _0x24f073,
+    participants: _0xd00b54,
+    isAdmin: _0x3c1ef3,
+    isOwner: _0x3b3686
+}) => {
+    const _0x4a9f73 = _0x8f22;
+    let _0x7fb002 = _0xd00b54[_0x4a9f73(0x1ef)](_0x12f964 => _0x12f964['id'])[_0x4a9f73(0x1ee)](_0x54a60a => _0x54a60a !== _0x1abe00['user'][_0x4a9f73(0x1e3)]);
+    _0x1a4457[_0x4a9f73(0x1db)]((_0x24f073 ? _0x24f073 + '\x0a' : '') + _0x4a9f73(0x1ed) + _0x7fb002[_0x4a9f73(0x1ef)](_0x5bd122 => _0x4a9f73(0x1e5) + _0x5bd122[_0x4a9f73(0x1f1)](/@.+/, ''))[_0x4a9f73(0x1e2)]
+        `\n` + _0x4a9f73(0x1d8), null, {
+            'mentions': _0x7fb002
+        });
+};
+handler[_0x12c300(0x1df)] = [_0x12c300(0x1e1)], handler['tags'] = ['group'], handler[_0x12c300(0x1ea)] = ['منشن'], handler[_0x12c300(0x1da)] = !![], handler[_0x12c300(0x1e9)] = !![];
+export default handler;
