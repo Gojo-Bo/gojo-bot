1ff61a37263b24daa1fd22e4a98a561b29cae71a
Gojo-Bo <omarmohamedhossny80@gmail.com>
2024-09-19 23:06:00 +0300

    Update تغير الخروج.js

--- plugins/تغير الخروج.js
+++ plugins/تغير الخروج.js
@@ -1,1 +1,11 @@
-
+let handler = async (m, { conn, text, iwelcimesROwner, isOwner }) => {
+if (text) {
+global.db.data.chats[m.chat].sBye = text
+m.reply('*تم تغيير رسالة المغادرة بنجاح [❗]*')
+} else throw `*تم تغيير رسالة الخروج بنجاح [❗],استخدم:*\n*- @user (منشن الشخص الذي غادر)*`
+}
+handler.help = ['<text>تغيرالمغادره']
+handler.tags = ['group']
+handler.command = ['تغيرالمغادره'] 
+handler.admin = true
+export default handler
