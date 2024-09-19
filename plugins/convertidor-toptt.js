13fdf81ee35044adfa1498085adbc4fcdafd6b65
Gojo-Bo <omarmohamedhossny80@gmail.com>
2024-09-19 12:44:30 +0300

    Update convertidor-toptt.js

--- plugins/convertidor-toptt.js
+++ plugins/convertidor-toptt.js
@@ -1,11 +1,18 @@
+import {toPTT} from '../lib/converter.js';
 const handler = async (m, {conn, usedPrefix, command}) => {
-  if (!m.quoted) throw `*[❗تنبيه❗] قم بالرد علي الفيديو اللي عايز تحولوا الي gif*`;
-  const q = m.quoted || m;
-  const mime = (q.msg || q).mimetype || '';
-  if (!/(mp4)/.test(mime)) throw `*[❗] النوع غير مناسب مع ${mime} الامر يعمل علي الفيديو فقط*`;
-  m.reply(global.wait);
-  const media = await q.download();
-  conn.sendMessage(m.chat, {video: media, gifPlayback: true, caption: '*تــم تـحـويـل الـفـيـديـو الي gif بـنـجاح*'}, {quoted: m});
+  const q = m.quoted ? m.quoted : m;
+  const mime = (m.quoted ? m.quoted : m.msg).mimetype || '';
+  if (!/video|audio/.test(mime)) throw `*[💾مساعده💾]قم بالرد علي الفيديو او الاغنيه اللي عايز تحولها لريكورد*`;
+  const media = await q.download?.();
+  if (!media && !/video/.test(mime)) throw '*الحجم كبير*';
+  if (!media && !/audio/.test(mime)) throw '*الحجم كبير*';
+  const audio = await toPTT(media, 'mp4');
+  if (!audio.data && !/audio/.test(mime)) throw '*[❗تحذير❗] حصل خطأ*';
+  if (!audio.data && !/video/.test(mime)) throw '*[❗تحذير❗] حصل خطأ*';
+  const aa = conn.sendFile(m.chat, audio.data, 'error.mp3', '', m, true, {mimetype: 'audio/mpeg'});
+  if (!aa) return conn.sendMessage(m.chat, {audio: {url: media}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
 };
-handler.command = ['جيف'];
+handler.help = ['tovn (reply)'];
+handler.tags = ['audio'];
+handler.command = /^لريك|to(vn)$/i;
 export default handler;
