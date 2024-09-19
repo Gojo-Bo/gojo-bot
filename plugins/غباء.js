420e8b19d29d4a13938b7e3e0a16387c727ac9c9
Gojo-Bo <omarmohamedhossny80@gmail.com>
2024-09-19 23:38:20 +0300

    Update غباء.js

--- plugins/غباء.js
+++ plugins/غباء.js
@@ -1,1 +1,10 @@
-
+let handler = async (m, { conn, command, text }) => {
+let stupidity = `*🤡 نسبة غباء 🤡*
+*نسبة اغباء ${text} 🤡هي* *${Math.floor(Math.random() * 100)}%* *من 100%*
+*ربنا يشفيك😂❤*
+`.trim()
+m.reply(stupidity, null, { mentions: conn.parseMention(stupidity) })}
+handler.help = ['stupidity']
+handler.tags = ['fun']
+handler.command = /^(غباء)$/i
+export default handler
