5ab675932080bc41ca12ae71af4ab1824bf92d7d
Gojo-Bo <omarmohamedhossny80@gmail.com>
2024-09-19 21:35:14 +0300

    Update احاديث.js

--- plugins/احاديث.js
+++ plugins/احاديث.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text}) => {
-m.reply(`*❆❯━━━❲Nano❳━━━━━❮❆*\n➢ *"${pickRandom(global.piropo)}"*\n*❆❯━━━❲Nano❳━━━━━❮❆*`)
+m.reply(`*❆❯━━━❲Gojo❳━━━━━❮❆*\n➢ *"${pickRandom(global.piropo)}"*\n*❆❯━━━❲Gojo❳━━━━━❮❆*`)
 }
 handler.tags = ['احاديث']
 handler.command = ['حديث','احاديث']
