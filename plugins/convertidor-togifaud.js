aab901444665aa4dfc4b68b83e879a5f6e2c5ebf
Gojo-Bo <omarmohamedhossny80@gmail.com>
2024-09-19 12:43:58 +0300

    Update convertidor-togifaud.js

--- plugins/convertidor-togifaud.js
+++ plugins/convertidor-togifaud.js
@@ -1,29 +1,11 @@
-import uploadImage from '../lib/uploadImage.js';
-const handler = async (m, {conn, text, args, usedPrefix, command}) => {
-  const q = m.quoted ? m.quoted : m;
-  const mime = (q.msg || q).mimetype || q.mediaType || '';
-  if (!/image/g.test(mime)) throw '*[❗] فين الصوره الي انت عايزها بشكل انمي*';
-  m.reply('*[❗] يتم تحويل الصوره لانمي، خلي عندك صبر عشان النتيجه*');
-  const data = await q.download?.();
-  const image = await uploadImage(data);
-  try {
-    const anime = `https://api.lolhuman.xyz/api/imagetoanime?apikey=${lolkeysapi}&img=${image}`;
-    await conn.sendFile(m.chat, anime, 'error.jpg', null, m);
-  } catch (i) {
-    try {
-      const anime2 = `https://api.zahwazein.xyz/photoeditor/jadianime?url=${image}&apikey=${keysxxx}`;
-      await conn.sendFile(m.chat, anime2, 'error.jpg', null, m);
-    } catch (a) {
-      try {
-        const anime3 = `https://api.caliph.biz.id/api/animeai?img=${image}&apikey=caliphkey`;
-        await conn.sendFile(m.chat, anime3, 'error.jpg', null, m);
-      } catch (e) {
-        throw '*[❗] خطا، وجه الشخص غير واضح*';
-      }
-    }
-  }
+const handler = async (m, {conn, usedPrefix, command}) => {
+  if (!m.quoted) throw `*[❗تنبيه❗] قم بالرد علي الفيديو اللي عايز تحولوا الي gif*`;
+  const q = m.quoted || m;
+  const mime = (q.msg || q).mimetype || '';
+  if (!/(mp4)/.test(mime)) throw `*[❗] النوع غير مناسب مع ${mime} الامر يعمل علي الفيديو فقط*`;
+  m.reply(global.wait);
+  const media = await q.download();
+  conn.sendMessage(m.chat, {video: media, gifPlayback: true, caption: '*تــم تـحـويـل الـفـيـديـو الي gif بـنـجاح*'}, {quoted: m});
 };
-handler.help = ['toanime'];
-handler.tags = ['tools'];
-handler.command = /^(jadianime|لانمي)$/i;
+handler.command = ['جيف'];
 export default handler;
