608b662b0f0ab0712f05af3871cb8fa9d98531da
Gojo-Bo <omarmohamedhossny80@gmail.com>
2024-09-19 22:52:02 +0300

    Update ترجمه.js

--- plugins/ترجمه.js
+++ plugins/ترجمه.js
@@ -1,1 +1,26 @@
-
+import translate from '@vitalets/google-translate-api'
+import fetch from 'node-fetch'
+let handler = async (m, { args, usedPrefix, command }) => {
+let msg = `*[❗ملاحظه❗] لاستخدام الامر عليك كتابة: ${usedPrefix + command} (اللغه) (النص)*\n*مثال:*\n*${usedPrefix + command} ar Hello*\n*للتعرف على اللغات المدعومة في:*\n*- https://cloud.google.com/translate/docs/languages*`
+if (!args || !args[0]) return m.reply(msg)  
+let lang = args[0]
+let text = args.slice(1).join(' ')
+const defaultLang = 'ar'
+if ((args[0] || '').length !== 2) {
+lang = defaultLang
+text = args.join(' ')}
+if (!text && m.quoted && m.quoted.text) text = m.quoted.text
+try {      
+let result = await translate(`${text}`, { to: lang, autoCorrect: true })
+await m.reply('*الترجمه:* ' + result.text)
+} catch {
+try {    
+let lol = await fetch(`https://api.lolhuman.xyz/api/translate/auto/${lang}?apikey=${lolkeysapi}&text=${text}`)
+let loll = await lol.json()
+let result2 = loll.result.translated
+await m.reply('*الترجمه:* ' + result2)
+} catch { 
+await m.reply('*[❗𝐈𝐍𝐅𝐎❗] ERROR, VUELVA A INTENTARLO*')    
+}}}
+handler.command = /^(ترجم|ترجمه|ترجمة)$/i
+export default handler
