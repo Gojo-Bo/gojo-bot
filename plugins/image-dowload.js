1bcb161bff904bd3d2a35f42f01cc03962770fe3
Gojo-Bo <omarmohamedhossny80@gmail.com>
2024-09-19 13:12:14 +0300

    Update image-dowload.js

--- plugins/image-dowload.js
+++ plugins/image-dowload.js
@@ -1,1 +1,16 @@
-
+import { googleImage } from '@bochilteam/scraper'
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) throw `*متنساش اللي انت عايز تبحث عليه يحب , مثال : .صوره كريستيانو*`
+const res = await googleImage(text)
+let image = res.getRandom()
+let link = image
+conn.sendHydrated(m.chat, `🔎 *البحث:* ${text}
+🌎 *المصدر:* Google
+`, author, link, link, '🔗 اللينك', null, null, [
+['🐦 التالي 🐦', `/imagen ${text}`]
+], m)
+}
+handler.help = ['gimage <query>', 'imagen <query>']
+handler.tags = ['internet', 'tools']
+handler.command = /^(صوره|image|بحث-صورة)$/i
+export default handler
