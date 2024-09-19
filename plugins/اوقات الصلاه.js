9b52ff4ef34831388f3bf87ecf8c665d851ed52f
Gojo-Bo <omarmohamedhossny80@gmail.com>
2024-09-19 22:42:37 +0300

    Update اوقات الصلاه.js

--- plugins/اوقات الصلاه.js
+++ plugins/اوقات الصلاه.js
@@ -1,1 +1,34 @@
-
+export async function before(m) {
+    this.autosholat = this.autosholat ? this.autosholat : {};
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender;
+    let name = await this.getName(who);
+    let id = m.chat;
+    if (id in this.autosholat) {
+        return false;
+    }
+    let jadwalSholat = {
+        الفجر: "04:30",
+        الضحى: "06:9",
+        الظهر: "01:02",
+        العصر: "16:30",
+        المغرب: "19:45",
+        العشاء: "21:22"
+    };
+    const date = new Date(new Date().toLocaleString("en-US", {
+      timeZone:"Africa/Cairo"
+    }));
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const timeNow = `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
+    for (const [sholat, waktu] of Object.entries(jadwalSholat)) {
+        if (timeNow === waktu) {
+            let caption = `السلام عليكم *${name}*,\nحان موعد صلاة *${sholat}* اذهب وتوضأ بسرعة وقم لصلاتك ♥ ولا تنسانا من الدعاء.\n\n*${waktu}*\n_هذا توقيت الصلاة في القاهرة وما جاورها_`;
+            this.autosholat[id] = [
+                this.reply(m.chat, caption, null),
+                setTimeout(() => {
+                    delete this.autosholat[id];
+                }, 57000)
+            ];
+        }
+    }
+}
