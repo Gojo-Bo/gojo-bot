8b6919a7ccce9dff606986a1d9243e448f82935a
Gojo-Bo <omarmohamedhossny80@gmail.com>
2024-09-19 13:10:43 +0300

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -1,1 +1,16 @@
+let handler = async (m, { conn, text, participants, isAdmin, isOwner }) => {
+  let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
+  m.reply(`${text ? `${text}
+*المنشن الجماعي*
+\n` + users.map(v => '│منشن 🫶🏻 ❤️@' + v.replace(/@.+/, '')).join`\n` + '\n*✦━━━━━━[ المحرز بوت ]━━━━━━✦*', null, {
+      mentions: users
+  })
+}
 
+handler.help = ['منشن']
+handler.tags = ['group']
+handler.command = ['منشن']
+handler.admin = true
+handler.group = true
+handler.botAdmin = true
+export default handler
