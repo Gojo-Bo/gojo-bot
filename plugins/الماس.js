6ce176726de54ad4423af2c2fb7e60668b849a50
Gojo-Bo <omarmohamedhossny80@gmail.com>
2024-09-19 22:22:22 +0300

    Update الماس.js

--- plugins/الماس.js
+++ plugins/الماس.js
@@ -1,1 +1,23 @@
+let handler = async (m, {conn, usedPrefix}) => {
 
+    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+    let user = global.db.data.users[who]
+    if (!(who in global.db.data.users)) throw `✳️ 
+المستخدم مفقود من قاعدة البيانات الخاصة بي`
+    conn.reply(m.chat, `
+┌───⊷ *BALANCE* ⊶
+▢ *📌الاسم* : _@${who.split('@')[0]}_
+▢ *💎عدد* : _${user.diamond}_
+▢ *⬆️XP* : _المجموع ${user.exp}_
+└──────────────
+
+*NOTA :* 
+يمكنك شراء 💎 الماس باستخدام الطلبات
+❏ *${usedPrefix}buy <cantidad>*
+❏ *${usedPrefix}buyall*`, m, { mentions: [who] })
+}
+handler.help = ['balance']
+handler.tags = ['econ']
+handler.command = ['bal', 'diamantes', 'diamond', 'الماس'] 
+
+export default handler
