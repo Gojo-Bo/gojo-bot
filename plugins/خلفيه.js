98386aca1733d67c5898ddf42b807b60cad090be
Gojo-Bo <omarmohamedhossny80@gmail.com>
2024-09-19 23:29:10 +0300

    Update خلفيه.js

--- plugins/خلفيه.js
+++ plugins/خلفيه.js
@@ -1,1 +1,9 @@
-
+import axios from "axios"
+let handler = async (m, {command, conn, usedPrefix}) => {
+let res = (await axios.get(`https://raw.githubusercontent.com/socona12/test/main/5lfya`)).data  
+let haha = await res[Math.floor(res.length * Math.random())]    
+conn.sendFile(m.chat, haha, 'error.jpg', ``, m)
+}
+handler.command = handler.help = ['خلفيه', 'خلفية']
+handler.tags = ['anime']
+export default handler
