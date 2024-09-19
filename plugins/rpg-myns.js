26c572ae0c516b45c8434d54fda7aaf3c37c8ed3
Gojo-Bo <omarmohamedhossny80@gmail.com>
2024-09-19 13:24:11 +0300

    Update rpg-myns.js

--- plugins/rpg-myns.js
+++ plugins/rpg-myns.js
@@ -1,1 +1,10 @@
-
+import { createHash } from 'crypto'
+let handler = async function (m, { conn, text, usedPrefix }) {
+let sn = createHash('md5').update(m.sender).digest('hex')
+await m.reply(`⬇️ *هذا هو الرقم التسلسلي الخاص بك * ⬇️`.trim())
+await m.reply(`${sn}`.trim())
+}
+handler.help = ['myns']
+handler.tags = ['xp']
+handler.command = /^(myns|الرقم-التسسلسي|رمزي|رمزى)$/i
+export default handler
