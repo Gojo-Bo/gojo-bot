3b21385daf0a3b48f981dfe61dfb76f9c526d7b8
Gojo-Bo <omarmohamedhossny80@gmail.com>
2024-09-19 23:40:14 +0300

    Update فعاليات.js

--- plugins/فعاليات.js
+++ plugins/فعاليات.js
@@ -1,1 +1,23 @@
+import axios from "axios"
+let handler = async (m, {command, conn, usedPrefix}) => {
+let res = (await axios.get(`https://raw.githubusercontent.com/socona12/TheMystic-Bot-MD/master/src/JSON/Anime-F3alya.json`)).data  
+let haha = await res[Math.floor(res.length * Math.random())]    
+conn.sendFile(m.chat, haha, 'error.jpg', `
+〄━━━━━⌬〔Gojo〕⌬━━━━━〄
 
+*⌬🕹️┋السوأل » احزر الشخصية*
+
+*⌬⏳┋الواقت » 60 ث*
+
+*⌬🏆┋الجائزه » 500k*
+
+*⌬━━──⧉⊰🎴⊱⧉──━━⌬*
+~⌬ تـوقيع المطور
+🤖┋«Omar»┋🤖
+*╰━━━━━━━━━━━━⬣*`, m)
+}
+handler.comm = handler.help = ['فعالة','فعا']
+handler.tags = ['game']
+handler.command = /^(فعاليات)$/i
+handler.admin = true
+export default handler
