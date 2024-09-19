20bc6942f7d4438160393a03f589b459ebb89f9d
Gojo-Bo <omarmohamedhossny80@gmail.com>
2024-09-19 23:05:17 +0300

    Update تطقيم.js

--- plugins/تطقيم.js
+++ plugins/تطقيم.js
@@ -1,1 +1,13 @@
-
+import fetch from 'node-fetch'
+let handler = async (m, { conn, command }) => {
+let res = await fetch(`https://api.lolhuman.xyz/api/random/ppcouple?apikey=${lolkeysapi}`)
+if (res.status != 200) throw await res.text()
+let json = await res.json()
+if (!json.status) throw json
+conn.sendButton(m.chat, 'بنت 👩‍💼', wm, json.result.female, [['🔄 الصورة التالية 🔄', `/${command}`]], m)
+conn.sendButton(m.chat, 'ولد 👨‍💼', wm, json.result.male, [['🔄 الصورة التالية 🔄', `/${command}`]], m)
+}
+handler.help = ['ppcouple']
+handler.tags = ['internet']
+handler.command = /^(طقم|تطقيم)$/i
+export default handler
