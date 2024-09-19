e3dc367b4367791f2466d211bc0954368c480dda
Gojo-Bo <omarmohamedhossny80@gmail.com>
2024-09-19 12:34:16 +0300

    Update buscador-yts.js

--- plugins/buscador-yts.js
+++ plugins/buscador-yts.js
@@ -1,1 +1,22 @@
+import yts from 'yt-search';
 
+const handler = async (m, {conn, text}) => {
+  if (!text) throw '⚠️ *_ما الذي تريد البحث عنه في يوتيوب؟_*';
+  const results = await yts(text);
+  const tes = results.all;
+  const teks = results.all.map((v) => {
+    switch (v.type) {
+      case 'video': return `
+° *_${v.title}_*
+↳ 🫐 *_الرابط :_* ${v.url}
+↳ 🕒 *_المده :_* ${v.timestamp}
+↳ 📥 *_التاريخ :_* ${v.ago}
+↳ 👁 *_المشاهدات :_* ${v.views}`;
+    }
+  }).filter((v) => v).join('\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n\n');
+  conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, m);
+};
+handler.help = ['ytsearch *<texto>*'];
+handler.tags = ['search'];
+handler.command = ['يوتيوب', 'yts'];
+export default handler;
