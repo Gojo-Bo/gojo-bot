215a0bd902b85e5c01383dd8ce113963ad0b1eac
Gojo-Bo <omarmohamedhossny80@gmail.com>
2024-09-19 23:05:41 +0300

    Update تغير الترحيب.js

--- plugins/تغير الترحيب.js
+++ plugins/تغير الترحيب.js
@@ -1,1 +1,11 @@
-
+let handler = async (m, { conn, text, isROwner, isOwner }) => {
+if (text) {
+global.db.data.chats[m.chat].sWelcome = text
+m.reply('*تم تغيير الترحيب بنجاح. [❗]*')
+} else throw `*[❗]يرجى كتابة رساله لتغير الترحيب, استخدم:*\n*- @user (منشن الشخص الذي دخل)*\n*- @group (اسم القروب)*\n*- @desc (وصف القروب)*`
+}
+handler.help = ['<text> تغيرالترحيب']
+handler.tags = ['group']
+handler.command = ['تغيرالترحيب'] 
+handler.admin = true
+export default handler
