e6a5b732e8245105bb802ede9cf7d78e0433a78b
Gojo-Bo <omarmohamedhossny80@gmail.com>
2024-09-19 13:21:38 +0300

    Update owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,1 +1,9 @@
-
+let handler = async (m) => {
+global.db.data.chats[m.chat].isBanned = false
+m.reply('*تم إلغاء حظر هذه الدردشة بنجاح*')
+}
+handler.help = ['unbanchat']
+handler.tags = ['owner']
+handler.command = /^فك-حظر|unbanchat|فك-الحظر$/i
+handler.rowner = true
+export default handler
