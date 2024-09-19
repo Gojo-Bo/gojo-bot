9c2e413d48be46519cc6881d619d974f36244d1e
Gojo-Bo <omarmohamedhossny80@gmail.com>
2024-09-19 13:20:20 +0300

    Update owner-leavegc.js

--- plugins/owner-leavegc.js
+++ plugins/owner-leavegc.js
@@ -1,1 +1,7 @@
-
+let handler = async (m, { conn, args, command }) => {
+await m.reply('*مش هتوحشوني مع السلامه اللي باعنا خسر دلعنا ⁦^⁠_⁠^⁩*') 
+await  conn.groupLeave(m.chat)}
+handler.command = /^(out|leavegc|اخرج|برا)$/i
+handler.group = true
+handler.rowner = true
+export default handler
