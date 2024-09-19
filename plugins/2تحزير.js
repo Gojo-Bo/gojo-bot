b99808ae868aa45881404e90874db8133b040daa
Gojo-Bo <omarmohamedhossny80@gmail.com>
2024-09-19 12:11:23 +0300

    Update 2تحزير.js

--- plugins/2تحزير.js
+++ plugins/2تحزير.js
@@ -1,1 +1,19 @@
-
+const handler = async (m, {conn, text, command, usedPrefix}) => {
+  const pp = './src/warn.jpg';
+  let who;
+  if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text;
+  else who = m.chat;
+  const user = global.db.data.users[who];
+  const bot = global.db.data.settings[conn.user.jid] || {};
+  const warntext = `*[❗] ضع علامة على شخص ما أو قم بالرد على رسالة جماعية*\n\n*مثال:*\n*${usedPrefix + command} @${global.suittag}*`;
+  if (!who) throw m.reply(warntext, m.chat, {mentions: conn.parseMention(warntext)});
+  if (m.mentionedJid.includes(conn.user.jid)) return;
+  if (user.warn == 0) throw '*[❗] المستخدم لديه 0 تحذيرات*';
+  user.warn -= 1;
+  await m.reply(`${user.warn == 1 ? `*@${who.split`@`[0]}*` : `♻️ *@${who.split`@`[0]}*`} تمت إزالة التحذير\n*تحذيرات ${user.warn}/3*`, null, {mentions: [who]});
+};
+handler.command = /^(شيل-التحذير|delwarn|deladvertir|deladvertencia|delwarning)$/i;
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
+export default handler;
