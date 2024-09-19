ac24e92acb10f46a35ed58b5496e4dbb84a6b954
Gojo-Bo <omarmohamedhossny80@gmail.com>
2024-09-19 13:22:51 +0300

    Update qrcode.js

--- plugins/qrcode.js
+++ plugins/qrcode.js
@@ -1,1 +1,9 @@
-
+import { toDataURL } from 'qrcode'
+let handler = async (m, { text, conn }) => {
+if (!text) throw `*حط الحاجه الي عايز تحولها باركود*`
+conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '¯\\_(BY:ALMOHREZ-BOT', m)
+}
+handler.help = ['', 'code'].map(v => 'qr' + v + ' <teks>')
+handler.tags = ['tools']
+handler.command = /^كود$/i
+export default handler
