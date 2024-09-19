9329d0948fe7c4915263889cc0b33ec591dec996
Gojo-Bo <omarmohamedhossny80@gmail.com>
2024-09-19 23:29:52 +0300

    Update درايف.js

--- plugins/درايف.js
+++ plugins/درايف.js
@@ -1,1 +1,37 @@
-
+import fetch from 'node-fetch'
+import { sizeFormatter } from 'human-readable'
+let formatSize = sizeFormatter({
+std: 'JEDEC', decimalPlaces: 2, keepTrailingZeroes: false, render: (literal, symbol) => `${literal} ${symbol}B` })
+let handler = async (m, { conn, args }) => {
+if (!args[0]) throw '*التحميل من غوغل درايف*' 
+try {
+GDriveDl(args[0]).then(async (res) => {
+conn.reply(m.chat, 'جاري تحميل الملف ارجوك يا صديقي تأكذ ان الملف الذي تريد تحميله لا يتجاوز 100 ميغا \n تابعني في الانستغرام \n instagram.com/noureddine_ouafy', m)
+if (!res) throw res
+conn.sendFile(m.chat, res.downloadUrl, res.fileName, '', m, null, { mimetype: res.mimetype, asDocument: true })})
+}catch(e){
+m.reply('*حذث خطأ راسل صاحب البوت ليصحح العطب*\n instagram.com/noureddine_ouafy')
+console.log(e)}}
+handler.command = /^(gdrive)$/i
+export default handler
+async function GDriveDl(url) {
+let id
+if (!(url && url.match(/drive\.google/i))) throw 'Invalid URL'
+id = (url.match(/\/?id=(.+)/i) || url.match(/\/d\/(.*?)\//))[1]
+if (!id) throw 'ID Not Found :( '
+let res = await fetch(`https://drive.google.com/uc?id=${id}&authuser=0&export=download`, {
+method: 'post',
+headers: {
+'accept-encoding': 'gzip, deflate, br',
+'content-length': 0,
+'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+'origin': 'https://drive.google.com',
+'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+'x-client-data': 'CKG1yQEIkbbJAQiitskBCMS2yQEIqZ3KAQioo8oBGLeYygE=',
+'x-drive-first-party': 'DriveWebUi',
+'x-json-requested': 'true'  }})
+let { fileName, sizeBytes, downloadUrl } =  JSON.parse((await res.text()).slice(4))
+if (!downloadUrl) throw 'Link Download Limit!'
+let data = await fetch(downloadUrl)
+if (data.status !== 200) throw data.statusText
+return { downloadUrl, fileName, fileSize: formatSize(sizeBytes), mimetype: data.headers.get('content-type')}}
